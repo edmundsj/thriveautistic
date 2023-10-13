c800117a98d4942f2b88a0362617a74a2b7ad741
Jordan <edmundsj@uci.edu>
2023-10-13 08:42:26 -0400

    added createTag helper function

--- frontend/data/tags.ts
+++ frontend/data/tags.ts
@@ -2,3 +2,11 @@ import {Insert, Row} from "@/data/generic";
 
 type Tag = Row<'tags'>
 type TagInsert = Insert<'tags'>
+export function createTag(tag:Partial<Tag>): Tag {
+  return {
+    id: 0,
+    created_at: '',
+    title: '',
+    ...tag
+  }
+}
